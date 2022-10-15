22f0145ac29eadc941b315178787dbd1aa9c0407
hieu22012003 <101488865+hieu22012003@users.noreply.github.com>
2022-10-15 17:11:50 +0700

    Update test.java

--- src/bienlai/test.java
+++ src/bienlai/test.java
@@ -2,12 +2,12 @@ package bienlai;
 
 public class test {
 	public static void main3(String[] args){
-        KhachHangVN a = new KhachHangVN(1,"Nguyen Trung Hieu","2022-10-15",219,"Cá nhân");
-        KhachHangNN b = new KhachHangNN(2,"Donal Trump","2022-09-21",156,"US");
+        KhachHangVN a = new KhachHangVN(1,"Nguyen Trung Hieu","2022-10-15",219,"ca nhan");
+        KhachHangNN b = new KhachHangNN(2,"Lionel Messi","2022-09-21",156,"US");
         System.out.println(a.thanhTien());
         System.out.println(b.thanhTien());
 
-        KhachHang kh = (KhachHang) new KhachHangVN(2,"Nguyen Van B","2022-09-22",100,"Cá nhân");
+        KhachHang kh = (KhachHang) new KhachHangVN(2,"Pham The Bach","2022-09-22",100,"ca nhan");
         System.out.println(kh.thanhTien());
 
         KhachHangVN khvn = (KhachHangVN)kh;
